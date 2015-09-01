de2852f71fbf079cad242e6deff3cb0117e2840d
Tamas Adam <tad@capraconsulting.no>
2015-09-01 22:12:47 +0200

    ConstructorProperties fix.

--- src/main/java/no/capra/bootcamp/spring/beandefinition/model/PersonBean.java
+++ src/main/java/no/capra/bootcamp/spring/beandefinition/model/PersonBean.java
@@ -3,6 +3,8 @@ package no.capra.bootcamp.spring.beandefinition.model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.beans.ConstructorProperties;
+
 /**
  * Created by tom on 01/09/15.
  */
@@ -19,6 +21,8 @@ public class PersonBean {
         logger.debug("Creating PersonBean with no-arg constructor");
     }
 
+
+    @ConstructorProperties({"firstName", "surName"})
     public PersonBean(String firstName, String surName){
         logger.debug("Creating PersonBean {} {}.", firstName, surName);
         this.firstName = firstName;
